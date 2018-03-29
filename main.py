acd1c29b5486ee8f34f2f5437d0c191ff471a9cb
shd <759275499@qq.com>
2018-03-29 18:41:59 +0800

    修改访问方式

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_script import Manager
 app = Flask(__name__)
 app.secret_key = '123'
-#manager = Manager(app)
+manager = Manager(app)
 
 from shd_view import admin
 app.register_blueprint(admin,url_prefix='/admin')
@@ -12,5 +12,5 @@ from login import login
 app.register_blueprint(login,url_prefix='/login')
 
 if __name__ == '__main__':
-	#manager.run()
-	app.run(host='0.0.0.0',port=5000,debug=True)
+	manager.run()
+	#app.run(host='0.0.0.0',port=5000,debug=True)
