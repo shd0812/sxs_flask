ed8bffb4080b820df9950dc52d673b5eff03dc3d
shd <759275499@qq.com>
2018-05-08 16:06:57 +0800

    新增

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template
 from flask_script import Manager
 app = Flask(__name__)
 app.secret_key = '123'
-manager = Manager(app)
+#manager = Manager(app)
 
 from shd_view import admin
 app.register_blueprint(admin,url_prefix='/admin')
@@ -16,5 +16,5 @@ def not_found(e):
     return render_template('404.html') 
 
 if __name__ == '__main__':
-	manager.run()
-	#app.run(host='0.0.0.0',port=5000,debug=True)
+	#manager.run()
+	app.run(host='0.0.0.0',port=5000,debug=True)
