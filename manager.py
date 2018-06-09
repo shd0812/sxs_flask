66041c2f162e219f2e85c55e8b97b5889bf5a1b1
shd <759275499@qq.com>
2018-06-09 10:11:33 +0800

    1.把星火项目和发标合并

--- manager.py
+++ manager.py
@@ -1,9 +1,11 @@
-#coding:utf-8
 from flask import Flask,render_template
-from flask_script import Manager
+
 app = Flask(__name__)
 app.secret_key = '123'
-#manager = Manager(app)
+from XennHo.XH_View import xnho
+app.register_blueprint(xnho,url_prefix='/home')
+
+
 
 from shd_view import admin
 app.register_blueprint(admin,url_prefix='/admin')
@@ -11,10 +13,10 @@ app.register_blueprint(admin,url_prefix='/admin')
 from login import login
 app.register_blueprint(login,url_prefix='/login')
 
-@app.errorhandler(404)  
-def not_found(e):  
-    return render_template('404.html') 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
 
 if __name__ == '__main__':
 	#manager.run()
-	app.run(host='0.0.0.0',port=5000,debug=True)
+	app.run(host='192.168.110.53',port=1314,debug=True)
