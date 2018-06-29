9e13435fbb2752fafaef7d7ee7944df24e9f5dd4
shd <759275499@qq.com>
2018-06-29 17:42:51 +0800

    修改Commom ，并修改目录

--- Common/common_Verify.py
+++ Common/common_Verify.py
@@ -1,7 +1,7 @@
 from common_OpFile import operate_File
 from common_Logger import *
 logger = myLog.getLog()
-
+import operator
 
 def eq_verify(check_data,path):
     if not path:
@@ -27,9 +27,6 @@ def eq_verify(check_data,path):
             elif isinstance(tmp,list):
                 tmp=tmp[int(result_data)]
 
-
-
-
         check_str=tmp
         expect_str = item.get(key)
         logger.info('要检查的字符{},期待的字符{}'.format(check_str,expect_str))
@@ -39,45 +36,39 @@ def eq_verify(check_data,path):
         else:
             raise Exception('不相等，要检查的是{},期望的是{}'.format(check_str,expect_str))
 
+def isExit_Key(expect_list,dic):
+    tmp_list=[]
+    flag=1
+    print(dic)
+    while flag==1:
+        tmp_dic=dic
+        for k, v in tmp_dic.items():
+            if isinstance(v,dict):
+                tmp_dic =v
+                flag =1
+            else:
+                tmp_list+=k
+    print(tmp_list)
 
 if __name__ == '__main__':
-    check_data={
-            'test_name':'用户信息查询',
-            'parm':{
-                'service':'bind_url',
-                'body':[{
-                    'index':{
-                        'name':'shen',
-                        'vals':[111111,222222],
-                        'sex' : 5
-                    }
-                    }]
-                }
+    tmp_dic = {
+        'name':'shen',
+        'sex': 5,
+        'province':'henan',
+        'vls': [1,2,3],
+        'index':{
+            'p':'t'
+        }
+    }
 
-            }
+    p_dic = {
+        'name':'shen',
+        'sex': 5,
+        'province':'henan',
+        'vls': [1,2,3],
+        'index':{
+            'p':'t'
+        }
+    }
 
-    #eq_verify(check_data,'')
-   # print('服务器返回{}'.format(check_data))
-    check_list=['parm.body.0.index.name','parm.body.0.index.sex']
-    #print(check_data)
-
-    for item in  range(len(check_list)):
-        s_item = check_list[item]
-        ss = s_item.split('.')
-        tmp = check_data
-       # print("=========11111===%s"%ss)
-
-        for x in range(len(ss)):
-            result_data = ss[x]
-            if isinstance(tmp,dict):
-
-                tmp=tmp.get(result_data)
-                print('xx:{}'.format(tmp))
-            elif isinstance(tmp,list):
-                tmp=tmp[int(result_data)]
-                print('yy: {}'.format(tmp))
-
-
-
-
-    #print(check_list)
+    isExit_Key([1],p_dic)
